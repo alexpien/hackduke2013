7202ac28fb94f6d0327f2d2b412e115a2e315fe5
alexpien <alexpien@gmail.com>
2013-11-16 17:23:03 -0500

    new rails

---
+++ pageshare/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pageshare::Application.config.secret_token = '6fb2b7490fc5a92e50536442459f82d26320dba932e735342c74fba47f0a91f5d879f7cb7f3fac6daf7f762cf1376adecedd6c357d3b4f1e5d415580cbe74cc4'
