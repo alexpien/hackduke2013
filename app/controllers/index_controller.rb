357d8893d40f983a5ab6d11e9d41bd08ab0f7aec
alexpien <alexpien@gmail.com>
2013-11-17 02:19:06 -0500

    fixed stash.post.first.url issues

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,6 @@
 class IndexController < ApplicationController
   def index
-  	@stashes=Stash.all.map{|x| x if x.posts.length>0}
-  	@stashes.sort_by{|e| -e["score"]}
+  	@stashes=Stash.order(:score)
   end
 
 end
