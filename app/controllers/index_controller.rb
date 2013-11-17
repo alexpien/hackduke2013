9a63ffa12340975dd86f7cefa9ffc5f34f26b009
Howard Chung <Howard@dyn-152-3-41-231.egr.duke.edu>
2013-11-17 02:08:05 -0500

    test

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,7 @@
 class IndexController < ApplicationController
   def index
   	@stashes=Stash.all.map{|x| x if x.posts.length>0}
-  	@stashes.sort_by{|e| -e[:score]}
+  	@stashes.sort_by{|e| -e["score"]}
   end
 
 end
