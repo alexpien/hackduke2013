14c032e97ad60e950fb30f9b7ccab6a9655471de
alexpien <alexpien@gmail.com>
2013-11-17 04:10:54 -0500

    like buttons working correctly

--- app/assets/javascripts/basic.js
+++ app/assets/javascripts/basic.js
@@ -1,19 +1,26 @@
 
 $( document ).ready(function(){
 
-    
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
 
     $(".like").click(function(){
-        $(this).toggleClass("is-liked");
+       
         var stash = $(this).attr("id").split("-")[1];
-        
+        $("#icon-"+stash).toggleClass("glyphicon-heart-empty");
+        $("#icon-"+stash).toggleClass("glyphicon-heart");
         console.log(stash);
         $.ajax({
             type: "POST",
             url: "/likes/stash",
             data: {"stash_id":stash},
             success: function(response){
-                $("#num-likes"+stash).text(response);
+                console.log(response["score"]);
+                
+                $("#num-likes-"+stash).text(response["score"]);
 }
         });
         
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,16 +7,16 @@ class LikesController < ApplicationController
         stash=Stash.find(sid)
 
         result=StashLike.where(user_id: uid, stash_id: sid)
-        if result.length>0:
-            result.destroy
-            stash.count-=1
+        if result.length>0
+            result[0].destroy
+            stash.score-=1
         else
             StashLike.create(user_id: uid, stash_id: sid)
-            stash.count+=1
+            stash.score+=1
         end
         stash.save
-        @json={}
-        @json["count"]=Stash.find(sid).score
-        render json: @result
+        response={}
+        response["score"]=Stash.find(sid).score
+        render json: response
     end
 end
