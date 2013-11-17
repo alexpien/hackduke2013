a0a8bd60cfa076a5a3f8511885ff96920eb3a68a
Howard Chung <Howard@dyn-152-3-41-231.egr.duke.edu>
2013-11-16 23:06:27 -0500

    fixed addbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-  require 'unirest'
   before_action :set_user, only: [:show]
-  before_action :signed_in, only: [:edit, :update]
+  before_action :signed_in, only: [:edit, :update, :newpost]
   before_action :correct_user, only: [:edit, :update]
 
   # GET /users/1
@@ -44,7 +43,6 @@ class UsersController < ApplicationController
   end
 
 def newpost
-
   current_user.posts.create(:url=>params[:url], :user_id=>current_user.id, :score=>0)
         redirect_to :back
   end
