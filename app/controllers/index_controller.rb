af604b58376d63f65f4070896888ca0c24963a64
alexpien <alexpien@gmail.com>
2013-11-17 11:36:51 -0500

    added recent

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,13 @@
 class IndexController < ApplicationController
   def index
+    @title="Popular Stashes"
   	@stashes=Stash.order(score: :desc).limit(8)
+  end
+
+  def recent
+    @title = "Recent Stashes"
+    @stashes = Stash.order(created_at: :desc).limit(8)
+    render 'index'
   end
 
 end
