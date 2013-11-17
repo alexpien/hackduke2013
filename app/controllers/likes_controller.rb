45c5560c75eddfe02432bfcbcddcb33e7aff132e
Howard Chung <Howard@dyn-152-3-41-231.egr.duke.edu>
2013-11-17 03:43:01 -0500

    updated likes method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,22 @@
 class LikesController < ApplicationController
       before_action :signed_in
-      
+
     def stash
         uid=current_user.id
         sid=params[:stash_id]
+        stash=Stash.find(sid)
+
         result=StashLike.where(user_id: uid, stash_id: sid)
         if result.length>0:
             result.destroy
+            stash.count-=1
         else
             StashLike.create(user_id: uid, stash_id: sid)
+            stash.count+=1
+        end
+        stash.save
+        @json={}
+        @json["count"]=Stash.find(sid).score
+        render json: @result
     end
 end
