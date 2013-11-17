bba61dd29c3f67f1f27e78c7f6a3fbdb4e1be4f8
Howard Chung <Howard@dyn-152-3-41-231.egr.duke.edu>
2013-11-17 00:08:15 -0500

    fixed bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131117025628) do
+ActiveRecord::Schema.define(version: 20131117044217) do
 
   create_table "posts", force: true do |t|
     t.integer  "user_id"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20131117025628) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image_path"
+  end
+
+  create_table "stashes", force: true do |t|
+    t.integer  "score"
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
